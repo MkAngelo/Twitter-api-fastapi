578847be870edb578044e77c19d80ef06a8306c6
MkAngelo <mikequi000@gmail.com>
2022-01-05 23:51:01 -0600

    Tweets Show All

--- main.py
+++ main.py
@@ -174,7 +174,24 @@ def update_a_user():
     tags=["Tweets"]
 )
 def home():
-    return {"Twitter API": "Working!"}
+    """
+    Home
+
+    This path operation shows all Tweets in the app
+
+    Parameters:
+    -
+    
+    Returns a json list with all tweets in the app, with the followin keys:
+    - tweet_id: UUID
+    - content: str
+    - created_at: datetime
+    - updated_at: Optional[datetime]
+    - by: User
+    """
+    with open("tweets.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
 
 ### Post a Tweet
 @app.post(
